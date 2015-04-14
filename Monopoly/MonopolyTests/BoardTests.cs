ad12fdf4771288de8fe59c7bf4d3644a4b57563d
Killfile <thegreatkillfile@gmail.com>
2015-04-14 00:04:58 -0400

    Initial Commit

---
+++ Monopoly/MonopolyTests/BoardTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monopoly;
+using NUnit.Framework;
+namespace Monopoly.Tests
+{
+    [TestFixture()]
+    public class BoardTests
+    {
+        Board board;
+        List<Space> spaces;
+
+        [SetUp]
+        public void SetupBoard()
+        {
+            spaces = new List<Space> {
+                new Space(),
+                new Space(),
+                new Space()
+            };
+
+            board = new Board(spaces);
+
+        }
+
+        [Test()]
+        public void BoardCanBeConstructed()
+        {
+            Assert.Pass();
+        }
+
+        [Test()]
+        public void AdvanceCanBeCalled()
+        {
+            var expected = spaces[2];
+            var actual = board.Advance(spaces[0], 2);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test()]
+        public void AdvanceLoops()
+        {
+            var expected = spaces[0];
+            var actual = board.Advance(spaces[0], spaces.Count);
+            Assert.That(actual, Is.EqualTo(expected));
+
+        }
+    }
+}
